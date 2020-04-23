aa05d2fe30e1a7c75e1b0c27ccb03281ceabc051
eszben <bence.eszterhai@gmail.com>
2020-04-23 19:13:53 +0200

    second modification

--- scripts.js
+++ scripts.js
@@ -1,4 +1,147 @@
-let name = 'Bence';
 
+ let name = 'Bence';
 console.log(name + "'s apples")
 console.log('Hello world')
+
+$('h1').text('We are having a break 10 minute break until 17:05');
+
+
+
+//{}
+
+
+
+// This is a multiline Javascript comment /*
+
+This is multiline Javascript comment. 
+
+*/
+
+
+// $This is the first part: 
+
+/*
+12+3
+15
+let age
+undefined
+age= 33
+33
+age/3
+11
+let legs = 4
+undefined
+legs+ 4
+8
+legs = 4
+4
+legs= 2
+2
+let name = Bence
+VM377:1 Uncaught ReferenceError: Bence is not defined
+    at <anonymous>:1:12
+(anonymous) @ VM377:1
+let anotherAge = age
+undefined
+ let nem = 'Bence'
+undefined
+name
+VM527:1 Uncaught ReferenceError: name is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM527:1
+let name = 'Bence'
+undefined
+name = "Peti"
+"Peti"
+"Bence's apples"
+"Bence's apples"
+let name 'something'
+VM770:1 Uncaught SyntaxError: Unexpected string
+let name = 'something'
+undefined
+age = 12
+12
+ahe
+VM859:1 Uncaught ReferenceError: ahe is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM859:1
+age
+12
+true
+true
+false
+false
+let oldEnoughToDrive = false;
+undefined
+oldEnoughToDrive = true
+true
+age + 5
+17
+name + 'else'
+"somethingelse"
+name + 'else'+14 +name + age
+"somethingelse14something12"
+NaN
+NaN
+false and true
+VM1227:1 Uncaught SyntaxError: Unexpected identifier
+!true
+false
+!oldEnoughToDrive
+false
+let color1 ='orange'
+undefined
+let color2 = 'blue'
+undefined
+let colors ['orange', 'blue', 'green', 'purple']
+VM1779:1 Uncaught SyntaxError: Unexpected token '['
+colors
+VM1793:1 Uncaught ReferenceError: colors is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM1793:1
+let colors = ['orange', 'blue', 'green', 'purple']
+undefined
+colors
+(4) ["orange", "blue", "green", "purple"]
+colors [3]
+"purple"
+colors.length
+4
+colors.constructor
+ƒ Array() { [native code] }
+colors.push('yellow')
+5
+colors
+(5) ["orange", "blue", "green", "purple", "yellow"]
+[12, 54,1212]
+(3) [12, 54, 1212]
+[colors, colors, colors]
+(3) [Array(5), Array(5), Array(5)]
+[12, 'hello', true]
+(3) [12, "hello", true]
+ [30, 'Bence' true]
+VM2196:1 Uncaught SyntaxError: Unexpected token 'true'
+let person {name:'Bence', age:30, oldEnoughToDrive:true}
+VM2267:1 Uncaught SyntaxError: Unexpected token '{'
+let person {name:'Bence', age:33, oldEnoughToDrive:true}
+VM2275:1 Uncaught SyntaxError: Unexpected token '{'
+let person = {name:'Bence', age:30, oldEnoughToDrive:true}
+undefined
+person
+{name: "Bence", age: 30, oldEnoughToDrive: true}
+person.name
+"Bence"
+person.age
+30
+name
+"something"
+age
+12
+oldEnoughToDrive
+true
+person.favouriteColors= ['lime','green']
+(2) ["lime", "green"]
+person
+{name: "Bence", age: 30, oldEnoughToDrive: true, favouriteColors: Array(2)}
+
+//
